0de03a34612880c02e9c1f516a43cc06a9a9ad08
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-01-20 10:27:48 +0100

    Add project files.

---
+++ Uppgift5.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift_5._1
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("hur många meningar vill du skriva in?");
+            int antal=int.Parse(Console.ReadLine());
+            string[] meningar = new string[antal];
+
+            for(int i= 0; i < antal; i++)
+            {
+                Console.WriteLine("skriv in en mening");
+                meningar[i] = Console.ReadLine();
+            }
+            Console.WriteLine("här är de menigarna du skrev");
+            for(int i=0; i < antal; i++)
+            {
+                Console.WriteLine($"\n{meningar[i]}");
+            }
+        }
+    }
+}
